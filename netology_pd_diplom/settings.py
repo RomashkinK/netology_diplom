26337e97ee732c8d2e73494ab8d3f3b4cf2c4560
RomashkinKirill <romashkin-92@ya.ru>
2024-10-27 16:36:51 +0300

    tests

--- netology_pd_diplom/settings.py
+++ netology_pd_diplom/settings.py
@@ -11,12 +11,16 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+# import django
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'netology_pd_diplom.settings')
+# django.setup()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '=hs6$#5om031nujz4staql9mbuste=!dc^6)4opsjq!vvjxzj@'
